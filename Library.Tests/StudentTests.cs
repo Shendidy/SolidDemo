a3766aa233fc8d36908d3c8a00bb347f4e053706
Sherif Shendidy <sherif@shendidy.com>
2020-08-12 22:07:34 +0100

    added Student tests

---
+++ Library.Tests/StudentTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Library.Tests
+{
+    class StudentTests
+    {
+        IStudent _student;
+        [SetUp]
+        public void Setup()
+        {
+            _student = new Student();
+        }
+        [Test]
+        public void SetName_GivenNewName_ShouldSetObjectsName()
+        {
+            //Arr
+            _student.SetName("Test Name");
+
+            //Act
+            var expected = "Test Name";
+            var actual = _student.name;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void SetAge_GivenNewName_ShouldSetObjectsName()
+        {
+            //Arr
+            _student.SetName("Test Name");
+
+            //Act
+            var expected = "Test Name";
+            var actual = _student.name;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
